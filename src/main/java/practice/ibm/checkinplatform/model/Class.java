3746929c21d1e046c0d3dc9653f7bf7e3f6aff0f
Ion Scripnic <asdfree03@gmail.com>
2021-07-18 18:12:22 +0300

    Created all entities

--- src/main/java/practice/ibm/checkinplatform/model/Class.java
+++ src/main/java/practice/ibm/checkinplatform/model/Class.java
@@ -1,4 +1,127 @@
 package practice.ibm.checkinplatform.model;
 
+import javax.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
 public class Class {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @ManyToOne
+    private User teacher;
+    private int year;
+    private String section;
+    @OneToMany(fetch = LAZY)
+    private List<User> students;
+    @ManyToOne
+    private Classroom classroom;
+    private LocalDate date;
+    private LocalTime time;
+    public Class() {
+    }
+
+    public Class(String name, User teacher, int year, String section, List<User> students, Classroom classroom, LocalDate date, LocalTime time) {
+        this.name = name;
+        this.teacher = teacher;
+        this.year = year;
+        this.section = section;
+        this.students = students;
+        this.classroom = classroom;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(User teacher) {
+        this.teacher = teacher;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public List<User> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<User> students) {
+        this.students = students;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public Classroom getClassroom() {
+        return classroom;
+    }
+
+    public void setClassroom(Classroom classroom) {
+        this.classroom = classroom;
+    }
+
+    @Override
+    public String toString() {
+        return "Class{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", teacher=" + teacher +
+                ", year=" + year +
+                ", section='" + section + '\'' +
+                ", students=" + students +
+                ", classroom=" + classroom +
+                '}';
+    }
 }
--- src/main/java/practice/ibm/checkinplatform/model/User.java
+++ src/main/java/practice/ibm/checkinplatform/model/User.java
@@ -1,4 +1,145 @@
 package practice.ibm.checkinplatform.model;
 
-public class User {
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String username;
+    private String password;
+    private String first_name;
+    private String last_name;
+    private int role;
+    @Basic(optional = true)
+    private int year;
+    @Basic(optional = true)
+    private String department;
+    @Basic(optional = true)
+    private String section;
+    @Basic(optional = true)
+    private String group;
+
+    public User() {
+    }
+
+    public User(String username, String password, String first_name, String last_name, int role) {
+        this.username = username;
+        this.password = password;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.role = role;
+    }
+
+    public User(String username, String password, String first_name, String last_name, int role, int year, String department, String section, String group) {
+        this.username = username;
+        this.password = password;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.role = role;
+        this.year = year;
+        this.department = department;
+        this.section = section;
+        this.group = group;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", role=" + role +
+                ", year=" + year +
+                ", department='" + department + '\'' +
+                ", section='" + section + '\'' +
+                ", group='" + group + '\'' +
+                '}';
+    }
 }
