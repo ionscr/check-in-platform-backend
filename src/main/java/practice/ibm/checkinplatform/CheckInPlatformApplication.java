ae2845b250252d602c54553820a6499b841eff29
Ion Scripnic <asdfree03@gmail.com>
2021-07-18 16:05:05 +0300

    spring boot project created

---
+++ src/main/java/practice/ibm/checkinplatform/CheckInPlatformApplication.java
@@ -0,0 +1,13 @@
+package practice.ibm.checkinplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckInPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckInPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practice/ibm/checkinplatform/CheckInPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.ibm.checkinplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckInPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
