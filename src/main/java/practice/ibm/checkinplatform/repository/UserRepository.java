0f534955a1c683f16c73ea4347f33fba37eb7b88
Ion Scripnic <asdfree03@gmail.com>
2021-07-21 00:54:48 +0300

    edited user repository and created user service

--- src/main/java/practice/ibm/checkinplatform/repository/UserRepository.java
+++ src/main/java/practice/ibm/checkinplatform/repository/UserRepository.java
@@ -10,5 +10,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByUsername(String username);
     Optional<User> findUsersByRole(int role);
+    Optional<User> findUserById(Long id);
 
 }
