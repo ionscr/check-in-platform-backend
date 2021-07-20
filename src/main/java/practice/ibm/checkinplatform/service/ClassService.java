774fd37370bd6a9005abbf6e3874d511ecd1cfe0
Ion Scripnic <asdfree03@gmail.com>
2021-07-21 01:06:39 +0300

    created Class service

---
+++ src/main/java/practice/ibm/checkinplatform/service/ClassService.java
@@ -0,0 +1,41 @@
+package practice.ibm.checkinplatform.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import practice.ibm.checkinplatform.model.User;
+import practice.ibm.checkinplatform.repository.ClassRepository;
+import practice.ibm.checkinplatform.repository.UserRepository;
+import practice.ibm.checkinplatform.model.Class;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class ClassService {
+    private final ClassRepository classRepository;
+
+    @Autowired
+    public ClassService(ClassRepository classRepository) {
+        this.classRepository = classRepository;
+    }
+
+    public Class addClass(Class class1) {
+        return classRepository.save(class1);
+    }
+
+    public List<Class> findAllClasses() {
+        return classRepository.findAll();
+    }
+
+    public Class updateClass(Class class1) {
+        return classRepository.save(class1);
+    }
+
+    public List<Class> findClassesByDate(LocalDate date) {
+        return classRepository.findClassesByDate(date);
+    }
+
+    public void deleteClassById(Long id){
+        classRepository.deleteClassById(id);
+    }
+}
