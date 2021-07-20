c7c6058c8b4305e9c6f78d054ec74682766557f6
Ion Scripnic <asdfree03@gmail.com>
2021-07-21 01:28:00 +0300

    created user resource

---
+++ src/main/java/practice/ibm/checkinplatform/resource/UserResource.java
@@ -0,0 +1,52 @@
+package practice.ibm.checkinplatform.resource;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import practice.ibm.checkinplatform.model.User;
+import practice.ibm.checkinplatform.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserResource {
+    private final UserService userService;
+
+    public UserResource(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users = userService.findAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+    @GetMapping("/find/{id}")
+    public ResponseEntity<User> getUserById (@PathVariable("id") Long id) {
+        User user = userService.findUserById(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        User newUser = userService.addUser(user);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+        User updateUser = userService.updateUser(user);
+        return new ResponseEntity<>(updateUser, HttpStatus.OK);
+    }
+    @GetMapping("/find/byRole/{role}")
+    public ResponseEntity<List<User>> getUsersByRole (@PathVariable("role") int role){
+        List<User> users = userService.findUsersByRole(role);
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+    @GetMapping("/find/byUName/{username}")
+    public ResponseEntity<User> getUserByUsername (@PathVariable("username") String username){
+        User user = userService.findUserByUsername(username);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+}
