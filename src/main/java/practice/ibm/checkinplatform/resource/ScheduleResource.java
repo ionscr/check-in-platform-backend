f8969a5e6da5ef96c8ae5d4f05dc19eebf1d6162
Ion Scripnic <asdfree03@gmail.com>
2021-07-25 01:37:27 +0300

    added schedule resource

---
+++ src/main/java/practice/ibm/checkinplatform/resource/ScheduleResource.java
@@ -0,0 +1,48 @@
+package practice.ibm.checkinplatform.resource;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import practice.ibm.checkinplatform.model.Schedule;
+import practice.ibm.checkinplatform.service.ScheduleService;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/feature")
+public class ScheduleResource {
+    private final ScheduleService scheduleService;
+
+    public ScheduleResource(ScheduleService scheduleService){
+        this.scheduleService = scheduleService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Schedule>> getAllSchedules(){
+        List<Schedule> schedules =scheduleService.findAllSchedules();
+        return new ResponseEntity<>(schedules, HttpStatus.OK);
+    }
+    @GetMapping("/date/{date}")
+    public ResponseEntity<List<Schedule>> getSchedulesByDate(@RequestBody Date date) {
+        List<Schedule> schedules = scheduleService.findSchedulesByDate(date);
+        return new ResponseEntity<>(schedules, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Schedule> addSchedule(@RequestBody Schedule schedule) {
+        Schedule newSchedule = scheduleService.addSchedule(schedule);
+        return new ResponseEntity<>(newSchedule, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Schedule> updateFeature(@RequestBody Schedule schedule) {
+        Schedule updateSchedule = scheduleService.updateSchedule(schedule);
+        return new ResponseEntity<>(updateSchedule, HttpStatus.OK);
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteFeature(@PathVariable("id") Long id) {
+        scheduleService.deleteScheduleById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
