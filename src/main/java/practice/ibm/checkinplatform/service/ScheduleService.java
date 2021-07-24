fa60112a33223a39e8cdcb20db3c2edf0dfed7fc
Ion Scripnic <asdfree03@gmail.com>
2021-07-25 01:31:45 +0300

    created schedule service

---
+++ src/main/java/practice/ibm/checkinplatform/service/ScheduleService.java
@@ -0,0 +1,37 @@
+package practice.ibm.checkinplatform.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import practice.ibm.checkinplatform.model.Schedule;
+import practice.ibm.checkinplatform.repository.ScheduleRepository;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class ScheduleService {
+    private final ScheduleRepository scheduleRepository;
+
+    @Autowired
+    public ScheduleService(ScheduleRepository scheduleRepository){
+        this.scheduleRepository = scheduleRepository;
+    }
+    public Schedule addSchedule(Schedule schedule) {
+        return scheduleRepository.save(schedule);
+    }
+
+    public List<Schedule> findAllSchedules() {
+        return scheduleRepository.findAll();
+    }
+
+    public Schedule updateSchedule(Schedule schedule) {
+        return scheduleRepository.save(schedule);
+    }
+
+    public List<Schedule> findSchedulesByDate(Date date) {
+        return scheduleRepository.findSchedulesByDate(date);
+    }
+    public void deleteScheduleById(Long id){
+        scheduleRepository.deleteScheduleById(id);
+    }
+}
