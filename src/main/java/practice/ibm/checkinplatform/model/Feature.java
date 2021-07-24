77bc3d10d2e8cb3c85720b00f80dc2708c2e6e1f
Ion Scripnic <asdfree03@gmail.com>
2021-07-25 01:08:16 +0300

    created feature service

--- src/main/java/practice/ibm/checkinplatform/model/Feature.java
+++ src/main/java/practice/ibm/checkinplatform/model/Feature.java
@@ -1,2 +1,17 @@
-package practice.ibm.checkinplatform.model;public class Feature {
+package practice.ibm.checkinplatform.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Feature {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "classroom_id", referencedColumnName = "id")
+    private Classroom classroom;
+    @Column(name = "feature" ,nullable = false)
+    private String feature;
+
 }
--- src/main/java/practice/ibm/checkinplatform/repository/FeatureRepository.java
+++ src/main/java/practice/ibm/checkinplatform/repository/FeatureRepository.java
@@ -1,2 +1,14 @@
-package practice.ibm.checkinplatform.repository;public interface FeatureRepository {
+package practice.ibm.checkinplatform.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import practice.ibm.checkinplatform.model.Classroom;
+import practice.ibm.checkinplatform.model.Feature;
+
+import java.util.List;
+
+@Repository
+public interface FeatureRepository extends JpaRepository<Feature, Long> {
+    void deleteFeatureById(Long id);
+    List<Feature> findFeaturesByClassroom(Classroom classroom);
 }
